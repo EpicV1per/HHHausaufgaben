9d8673c276b169a77fdcb244b70cec6a5bdbef28
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-11-13 13:07:45 +0100

    package update

--- Aijdin/VierGewinnt.java
+++ Aijdin/VierGewinnt.java
@@ -1,3 +1,5 @@
+package HHHausaufgaben.Aijdin;
+
 public class VierGewinnt {
     public static void main(String[] args) {
     }
@@ -22,5 +24,5 @@ public class VierGewinnt {
         }
     }
 
-    
+
 }
