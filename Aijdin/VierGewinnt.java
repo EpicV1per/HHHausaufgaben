e16a76eb569c56def143d2daa50e261030caeed0
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-11-13 14:57:09 +0100

    insertToken implemented

--- Aijdin/VierGewinnt.java
+++ Aijdin/VierGewinnt.java
@@ -2,6 +2,13 @@ package HHHausaufgaben.Aijdin;
 
 public class VierGewinnt {
     public static void main(String[] args) {
+        char[][] sampleField = {
+            {'X', 'O', '_'},
+            {'O', 'X', '_'},
+            {'X', 'O', 'O'}
+        };
+        insertToken(sampleField, 'X', 2);
+        printField(sampleField);
     }
 
     /*
@@ -21,6 +28,34 @@ public class VierGewinnt {
                 System.err.print(cell + " ");
             }
             System.err.println();
+        }
+    }
+
+    /*
+        Task:
+            Insert Token function, inserte gegeben char token in gegebenen int index
+        Solution:   
+            Iterate over given column in each row checking if it contain an "empty" field,
+            when on an empty field replace with given char token.
+        Pseudocode: 
+            for each row
+                if column index of given row contain "_"
+                    replace with given token
+                    exit
+                else
+
+     */
+    
+    public static void insertToken(char[][] field, char token, int index) {
+        try {
+            for (int counter = field.length - 1; counter >= 0; counter--) {
+                if (field[counter][index] == '_') {
+                    field[counter][index] = token;
+                    break;
+                }
+            }
+        } catch (Exception a) {
+            System.out.print("Column already full");
         }
     }
 
