e4ca64059531871d50831c46c7833c0bbd53542f
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-11-13 13:01:27 +0100

    Update VierGewinnt.java

--- Aijdin/VierGewinnt.java
+++ Aijdin/VierGewinnt.java
@@ -1,27 +1,18 @@
 public class VierGewinnt {
-/*
-    Für diesen code musst du durch jede Reihe und Zeile iteraten
-    um die Elemente der Arrays zu drucken
-
-    Pseudocode:
-    for each row
-        for each column
-            print element
-        next row
-    exit
- */
-
-
-
     public static void main(String[] args) {
-        char[][] sampleField = {
-            {'X', 'O', 'X'},
-            {'O', 'X', 'O'},
-            {'X', 'O', 'X'}
-        };
-        printField(sampleField);
     }
 
+    /*
+        Für diesen code musst du durch jede Reihe und Zeile iteraten
+        um die Elemente der Arrays zu drucken
+
+        Pseudocode:
+        for each row
+            for each column
+                print element
+            next row
+        exit
+    */
     public static void printField(char[][] field) {
         for (char[] row : field) {
             for (char cell : row) {
@@ -30,4 +21,6 @@ public class VierGewinnt {
             System.err.println();
         }
     }
+
+    
 }
