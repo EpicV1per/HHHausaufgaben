a5d9b4ea4b2c365e0ae5e513d6ca84e0ff169b79
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-11-13 12:59:27 +0100

    Update PenguPowers.py

--- Sami/PenguPowers.py
+++ Sami/PenguPowers.py
@@ -1,3 +1,16 @@
+"""
+    Task: Gegeben int x und int i, berechne x^i ohne *-Operator zu benutzen.
+    Solution: For i number of times add x to temp, effectively multiplying it.
+    Pseudocode:
+    set answer 1 for case i = 0
+        for i
+            reset temp
+            for x
+                add answer to temp
+            set answer to temp
+        return answer
+"""
+
 def PenguPowers(x, i):
     answer = 1
     for num1 in range(i):
